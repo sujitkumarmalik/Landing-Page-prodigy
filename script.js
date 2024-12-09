82f7294003ac8e81ca89e1b10a42e2501200deaa
SUJIT KUMAR MALIK <157126586+sujitkumarmalik@users.noreply.github.com>
2024-12-09 19:51:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select the navbar
+const navbar = document.querySelector('.navbar');
+
+// Add an event listener to track scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
